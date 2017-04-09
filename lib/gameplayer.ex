ef98b47e2d8c5790d353463cd93743b625b2506e
Robert Jan Zwetsloot <zwetsloot.robert.jan@techcross.co.jp>
2017-04-09 15:33:15 +0900

    initialized gameplayer repository

---
+++ lib/gameplayer.ex
@@ -0,0 +1,18 @@
+defmodule Gameplayer do
+  @moduledoc """
+  Documentation for Gameplayer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gameplayer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gameplayer_test.exs
@@ -0,0 +1,8 @@
+defmodule GameplayerTest do
+  use ExUnit.Case
+  doctest Gameplayer
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
